30ada8a62ee1e61c47647d344bf68fb95a2cba96
anluu275 <56841742+anluu275@users.noreply.github.com>
2019-11-13 20:28:10 -0800

    Add files via upload
    eval express

---
+++ evalexpr/ex00/ft_atoi.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anluu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/28 13:49:58 by anluu             #+#    #+#             */
+/*   Updated: 2019/11/09 19:25:41 by anluu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+int		positive(char *str)
+{
+	int		result;
+
+	result = 0;
+	if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result);
+}
+
+int		negative(char *str)
+{
+	int		sign;
+	int		result;
+
+	sign = -1;
+	result = 0;
+	str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	result = result * sign;
+	return (result);
+}
+
+int		ft_atoi(char *str)
+{
+	while (*str != '\0')
+	{
+		while (*str > 33 && *str != 127)
+		{
+			if (*str == '+')
+			{
+				return (positive(str));
+			}
+			if (*str == '-')
+			{
+				return (negative(str));
+			}
+			else
+			{
+				return (positive(str));
+			}
+		}
+		str++;
+	}
+	return (0);
+}
---
+++ evalexpr/ex00/ft_list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anluu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/09 19:20:35 by anluu             #+#    #+#             */
+/*   Updated: 2019/11/09 19:24:39 by anluu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void ft_putchar(char c);
+void ft_putnbr(int nb);
+int	ft_atoi(char *str);
+int	eval_expr(char *str);
+
+#endif
